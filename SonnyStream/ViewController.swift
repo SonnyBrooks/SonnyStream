f4592224de61aee0fd7ee13a850452573de57ad2
Andrew Budziszek <abudziszek@gmail.com>
2014-12-23 21:35:59 -0600

    Initial Commit

---
+++ SonnyStream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SonnyStream
+//
+//  Created by Andy Budziszek on 12/23/14.
+//  Copyright (c) 2014 Andy Budziszek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
